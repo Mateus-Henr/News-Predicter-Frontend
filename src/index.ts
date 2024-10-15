a612636e77df40783c184022a92e1ad6448f5bf0
matt <mateush.study@gmail.com>
2024-10-15 15:13:46 -0300

    test: puppeteer params

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,8 @@ const client = new Client({
         headless: true,
         args: [
             "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--disable-dev-shm-usage",
+            // "--disable-setuid-sandbox",
+            // "--disable-dev-shm-usage",
             // "--disable-accelerated-2d-canvas",
             // "--disable-gpu"
         ],
